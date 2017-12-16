e7acbcfb0bd2f74a67d55050305c8c757d718bdc
Adalberto Taylor <adalbertotaylor84@gmail.com>
2017-12-16 17:53:28 -0200

    Imported Normalize

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 
 import { Hero } from './components/Hero'
+import { DateChoser } from './components/DateChooser'
+import { HotelsList } from './components/HotelsList'
 
 export class App extends Component {
   render() {
     return (
-      <Hero />
+      <div>
+        <Hero />
+        <DateChoser />
+        <HotelsList />
+      </div>
     );
   }
 }
