cdbe4cf01cd1cbbc46a3bb0d67656993076eff33
Adalberto Taylor <adalbertotaylor84@gmail.com>
2017-12-16 18:09:46 -0200

    Added HotelsFilter component

---
+++ src/components/HotelsFilter.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import { PriceSlider } from './PriceSlider'
+import { StarsFilter } from './StarsFilter'
+import { FilterWrapper } from './styled/HotelsFilterStyled'
+
+export class HotelsFilter extends Component {
+  render() {
+    return (
+      <FilterWrapper>
+          <h3 className="title">Filters</h3>
+          <PriceSlider />
+          <div className='divider'></div>
+          <StarsFilter />
+      </FilterWrapper>
+    )
+  }
+}
---
+++ src/components/styled/HotelsFilterStyled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const FilterWrapper = styled.div`
+  width: 291px;
+  height: 200px;
+
+  .title {
+    font-family: 'Montserrat';
+    font-weigth: normal;
+    font-size: 22px;
+    letter-spacing: 2.5px;
+  }
+
+  .label {
+    font-family: 'Heebo';
+    font-weight: normal;
+    font-size: 12.8px;
+    color: #B5B5B5;
+  }
+`
