9b4ae66555d76049765f64f387e5b736ced240b7
Adalberto Taylor <adalbertotaylor84@gmail.com>
2017-12-17 13:40:44 -0200

    Added Toast to notify when the user tries to search without setting the dates

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import moment from 'moment'
+import { ToastContainer, toast } from 'react-toastify'
 
 import { setDates } from '../modules/dates'
 import { searchHotels } from '../modules/hotels'
@@ -20,6 +21,32 @@ const mapStateToProps = state => {
 }
 
 class DatePicker extends Component {
+  constructor(props) {
+    super(props)
+    this.search = this.search.bind(this)
+    this.notify = this.notify.bind(this)
+  }
+
+  search() {
+    const { from, to } = this.props
+    if(from !== '' && to !== '') {
+      this.props.searchHotels()
+    } else {
+      this.notify()
+    }
+  }
+
+  notify() {
+    if(!toast.isActive(this.toastId)) {
+      this.toastId = toast.error('Select check-in and check-out dates', {
+        position: toast.POSITION.BOTTOM_CENTER,
+        autoClose: 10000
+      })
+    }
+  }
+
+  toastId = null
+
   formatDate(date) {
     return (
       <span>
@@ -48,13 +75,14 @@ class DatePicker extends Component {
                 <div className='title'>CHECK-OUT</div>
                 <div className='date'>{to !== '' ? this.formatDate(to) : 'Select check-out date'}</div>
               </DateOptionsSelected>
-              <DateOptionsButton onClick={this.props.searchHotels}>
+              <DateOptionsButton onClick={this.search}>
                 Search hotels
               </DateOptionsButton>
             </DateOptionsWrapper>
             <Calendar />
           </ActionsWrapper>
         </DateContainer>
+        <ToastContainer pauseOnHover={false} />
       </DateWrapper>
     )
   }
