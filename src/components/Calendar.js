dd9b38433869b091bb71820f09c7269fa6ab9864
Adalberto Taylor <adalbertotaylor84@gmail.com>
2017-12-17 10:59:28 -0200

    Connected Calendar to Redux

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import moment from 'moment'
 import _ from 'lodash'
+
+import { setDates } from '../modules/dates'
 
 import { CalendarWrapper, CalendarRow, CalendarLabels, CalendarDays } from './styled/CalendarStyled'
 
 const fetchMonth = () => {
-  const month = moment().startOf('month')
-  const lastDayOfMonth = moment().endOf('month').format('D')
+  const month = moment('2017-08-01').startOf('month')
+  const lastDayOfMonth = moment('2017-08-01').endOf('month').format('D')
 
   const cal = {
     month: month.format('MMMM'),
@@ -23,7 +27,7 @@ const fetchMonth = () => {
       weekDay: month.format('dd').substring(0,1),
       date: month.format()
     }
-    week[month.day()] = month.format('D')
+    week[month.day()] = month.format()
     cal.dates.push(day)
     if(month.day() === 6 || month.format('D') === lastDayOfMonth) {
       cal.weeks.push(week)
@@ -35,11 +39,53 @@ const fetchMonth = () => {
   return cal
 }
 
-export class Calendar extends Component {
+const mapDispatchToProps = dispatch => bindActionCreators({
+  setDates
+}, dispatch)
+
+const mapStateToProps = state => {
+  const { from, to } = state.dates
+  return { from, to }
+}
+
+class Calendar extends Component {
   constructor (props) {
     super(props)
     this.state = {
       cal: []
+    }
+
+    this.pickDate = this.pickDate.bind(this)
+  }
+
+  pickDate(date) {
+    const { from, to } = this.props
+    const datePicked = moment(date)
+    if(!from) {
+      this.props.setDates({
+        from: datePicked
+      })
+    } else if(from && !to) {
+      if(datePicked.isSame(from)) {
+        this.props.setDates({
+          from: '',
+          to: ''
+        })
+      } else if(datePicked.isBefore(from)) {
+        this.props.setDates({
+          from: datePicked,
+          to: from
+        })
+      } else {
+        this.props.setDates({
+          to: datePicked
+        })
+      }
+    } else if(from && to) {
+      this.props.setDates({
+        from: datePicked,
+        to: ''
+      })
     }
   }
 
@@ -59,9 +105,20 @@ export class Calendar extends Component {
   }
 
   renderWeek(week, index) {
+    const { from, to } = this.props
     return(
       <CalendarRow key={index}>
-        {week.map((day, index) => <CalendarDays key={index}>{day}</CalendarDays>)}
+        {week.map((day, index) => 
+          <CalendarDays
+            key={index}
+            onClick={() => this.pickDate(day)}
+            className={
+              moment(day).isSame(from) ? !to ? 'from only' : 'from' : 
+                moment(day).isBetween(from, to) ? 'interval' : 
+                  moment(day).isSame(to) ? 'to' : null}
+          >
+            <a>{day !== ' ' ? moment(day).format('D') : ' '}</a>
+          </CalendarDays>)}
       </CalendarRow>
     )
   }
@@ -80,3 +137,8 @@ export class Calendar extends Component {
     )
   }
 }
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Calendar)
